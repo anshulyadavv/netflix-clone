219aa1c9a6452a6a5e1c9e84754b4d843be645c1
anshulyadavv <anshulyadav876@gmail.com>
2025-04-01 01:40:13 +0530

    added home page banner and tiles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt="" />
+      </div>
     </div>
   )
 }
