950f05010b7baf14f2d099d41f4e91ad25074930
anshulyadavv <anshulyadav876@gmail.com>
2025-04-01 19:14:02 +0530

    added title cards and modified other features

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="Facebook" />
+        <img src={instagram_icon} alt="Instagram" />
+        <img src={twitter_icon} alt="Twitter" />
+        <img src={youtube_icon} alt="Youtube" />
+      </div>
     </div>
   )
 }
