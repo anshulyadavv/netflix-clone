bf35a5e4d946dc28e0987fab3ebc36c5f256ef23
anshulyadavv <anshulyadav876@gmail.com>
2025-04-02 21:27:09 +0530

    added login page and fixed bugs in the home page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,21 @@ const Footer = () => {
         <img src={twitter_icon} alt="Twitter" />
         <img src={youtube_icon} alt="Youtube" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>© 1997-2025 Netflix, Inc.</p>
     </div>
   )
 }
