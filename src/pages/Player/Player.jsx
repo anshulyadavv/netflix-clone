3cc9b3366cc38bfb575f9bad577bb15139b90560
anshulyadavv <anshulyadav876@gmail.com>
2025-04-07 00:30:05 +0530

    home page and added autoplayer

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='Player'>
-      
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/qSEeQXzFspk' title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
     </div>
   )
 }
