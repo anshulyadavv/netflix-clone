ac543820fbb8a76b3202abe9ca8502e5f8ef30a1
anshulyadavv <anshulyadav876@gmail.com>
2025-04-07 02:10:08 +0530

    added firebase auth and updated files

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup } from '../../firebase'
 
 const Login = () => {
 
 
-  const [signState, setSignState] = useState("Sign In")
+  const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = async (event)=>{
+    event.preventDefault();
+    if(signState==="Sign In"){
+      await login(email, password);
+    } else{
+      await signup(name, email, password);
+    }
+  }
+
   return (
 
      <div className='login'>
@@ -13,11 +27,11 @@ const Login = () => {
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState==="Sign Up"? <input type="text" placeholder="Your Name"/>:<></>}
+          {signState==="Sign Up"? <input value={name} onChange={(e)=>{(setName(e.target.value))}} type="text" placeholder="Your Name"/>:<></>}
           
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
-          <button>{signState}</button>
+          <input value={email} onChange={(e)=>{(setEmail(e.target.value))}} type="email" placeholder="Email"/>
+          <input value={password} onChange={(e)=>{(setPassword(e.target.value))}} type="password" placeholder="Password"/>
+          <button onClick={user_auth} type='submit'>{signState}</button>
           <div className="form-help">
             <div className="remember-me">
               <input type="checkbox"/>
@@ -32,7 +46,7 @@ const Login = () => {
           :<p>Already have account? <span onClick={()=>{setSignState("Sign In")}}>Sign In</span></p>
         }
           
-                  </div>
+        </div>
       </div>
     </div>
   )
